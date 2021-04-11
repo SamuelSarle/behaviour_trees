d647b77cfa0082e40b9acdf0ff7ae34a2f8fd0e9
Samuel Sarle <samuel@sarle.com>
2021-04-12 00:18:06 +0300

    Initial commit.

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject behaviour_trees "0.1.0-SNAPSHOT"
+  :description "Building blocks for creating behaviour trees."
+  :url "https://github.com/samuelsarle/behaviour_trees"
+  :license {:name "Unlicense"
+            :url "https://unlicense.org/"}
+  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :repl-options {:init-ns behaviour-trees.core})
---
+++ src/behaviour_trees/core.clj
@@ -0,0 +1,54 @@
+(ns behaviour-trees.core)
+
+(defn execute
+  ([] true)
+  ([x] (eval x)))
+
+(defmacro randomized-order
+  [f & args]
+   `(~f #(identity (execute %)) (shuffle '(~@args))))
+
+(defmacro sequence
+  ([x] x)
+  ([x & rest]
+   `(and ~x ~@rest)))
+
+(defmacro rand-sequence
+  ([x] x)
+  ([x & rest]
+   `(randomized-order every? ~x ~@rest)))
+
+(defmacro selector
+  ([x] x)
+  ([x & rest]
+   `(or ~x ~@rest)))
+
+(defmacro rand-selector
+  ([x] x)
+  ([x & rest]
+   `(randomized-order some ~x ~@rest)))
+
+(defmacro until
+  [pred]
+  `(loop []
+     (if ~pred
+       true
+       (recur))))
+
+(defmacro until-success
+  [x]
+  `(until (true? ~x)))
+
+(defmacro until-failure
+  [x]
+  `(until (false? ~x)))
+
+(defmacro inverter
+  [x]
+  `(not ~x))
+
+(defmacro succeeder
+  [x]
+  `(do
+     ~x
+     true))
