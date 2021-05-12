4ecb3436722fe99c2d756167bffc9d4d58b8af5d
Samuel Sarle <samuel@sarle.com>
2021-05-12 22:44:39 +0300

    Added failer macro.

--- src/behaviour_trees/core.clj
+++ src/behaviour_trees/core.clj
@@ -1,12 +1,12 @@
-(ns behaviour-trees.core)
+(ns behaviour-trees.core
+  (:refer-clojure :exclude [sequence]))
 
-(defn execute
+(defn exec
   ([] true)
   ([x] (eval x)))
 
-(defmacro randomized-order
-  [f & args]
-   `(~f #(identity (execute %)) (shuffle '(~@args))))
+(defmacro randomized-order [f & args]
+   `(~f #(identity (exec %)) (shuffle '(~@args))))
 
 (defmacro sequence
   ([x] x)
@@ -28,27 +28,27 @@
   ([x & rest]
    `(randomized-order some ~x ~@rest)))
 
-(defmacro until
-  [pred]
+(defmacro inverter [x]
+  `(not ~x))
+
+(defmacro succeeder [x]
+  `(do
+     ~x
+     true))
+
+(defmacro failer [x]
+  `(inverter (succeeder ~x)))
+
+(defmacro until [pred]
   `(loop []
      (if ~pred
        true
        (recur))))
 
-(defmacro until-success
-  [x]
+(defmacro until-success [x]
   `(until (true? ~x)))
 
-(defmacro until-failure
-  [x]
-  `(until (not (true? ~x))))
+(defmacro until-failure [x]
+  `(until (inverter (true? ~x))))
 
-(defmacro inverter
-  [x]
-  `(not ~x))
 
-(defmacro succeeder
-  [x]
-  `(do
-     ~x
-     true))
